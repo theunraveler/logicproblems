b842e51b328be794f76a8b1ede24d8e0609344b3
Jake Bell <jacobbell@fastmail.com>
2025-07-09 00:49:47 -0500

    s/capitalize/title-case/

--- src/main.ts
+++ src/main.ts
@@ -3,7 +3,7 @@ import './assets/main.css'
 import { createApp } from 'vue'
 import { createBootstrap } from 'bootstrap-vue-next'
 import { default as numberToWords } from 'number-to-words'
-import * as capitalize from 'capitalize'
+import { titleCase } from 'title-case'
 import problems from './data/problems.json' with {type: 'json'}
 import App from './App.vue'
 import router from './router.ts'
@@ -18,7 +18,7 @@ app.provide('chapters', Object.fromEntries(
   [...new Set(Object.values(problems).map((p) => p.chapter))]
   .sort()
   .filter((c) => !!c)
-  .map((c) => [c, `Chapter ${capitalize.words(numberToWords.toWords(c))}`])
+  .map((c) => [c, `Chapter ${titleCase(numberToWords.toWords(c))}`])
 ));
 
 app.mount('#app')
