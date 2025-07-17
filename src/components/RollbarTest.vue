01ba1ce8ab621de91bf3f1c11cbd65b294d5133b
Jake Bell <jacobbell@fastmail.com>
2025-07-16 23:41:11 -0500

    Rollbar

---
+++ src/components/RollbarTest.vue
@@ -0,0 +1,13 @@
+<script setup lang="ts">
+const triggerError = () => {
+  throw new Error('Testing Rollbar integration')
+}
+</script>
+
+<template>
+  <div>
+    <BButton variant="secondary" size="sm" @click="triggerError">
+      Test Rollbar
+    </BButton>
+  </div>
+</template>
