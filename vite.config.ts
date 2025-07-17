f5f4536963bb0583214d222e19f1314970a2b079
Jake Bell <jacobbell@fastmail.com>
2025-07-16 23:38:59 -0500

    Removing jsx support

--- vite.config.ts
+++ vite.config.ts
@@ -1,19 +1,15 @@
 import { fileURLToPath, URL } from 'node:url'
-
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
-import vueJsx from '@vitejs/plugin-vue-jsx'
 import vueDevTools from 'vite-plugin-vue-devtools'
 import Icons from 'unplugin-icons/vite'
 import Components from 'unplugin-vue-components/vite'
 import { BootstrapVueNextResolver } from 'bootstrap-vue-next'
 import IconsResolve from 'unplugin-icons/resolver'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [
     vue(),
-    vueJsx(),
     vueDevTools(),
     Components({
       resolvers: [BootstrapVueNextResolver(), IconsResolve()],
