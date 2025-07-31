f2afb6c40856e6889bca1e64b6682057a318e311
Jake Bell <jacobbell@fastmail.com>
2025-07-31 03:02:16 -0500

    Prompting for persistent storage

--- src/store.ts
+++ src/store.ts
@@ -8,3 +8,13 @@ export const db = new Dexie('logicproblems') as Dexie & {
 db.version(1).stores({
   solutions: '++id, problemId, completedAt',
 })
+
+/****
+ * Helper functions for initiating persistent storage.
+ * Ripped from https://dexie.org/docs/StorageManager.
+ */
+await (async () => {
+  return (await navigator.storage) && navigator.storage.persist
+    ? navigator.storage.persist()
+    : undefined
+})()
