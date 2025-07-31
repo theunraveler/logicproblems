62061d0e620641c9f444049ba878d825b58ef0da
Jake Bell <jacobbell@fastmail.com>
2025-07-31 16:59:27 -0500

    Adding button to export indexeddb data

--- src/components/AppFooter.vue
+++ src/components/AppFooter.vue
@@ -1,12 +1,23 @@
 <script setup lang="ts">
+import { exportDB } from 'dexie-export-import';
+import download from 'downloadjs'
+import { db } from '@/store'
+
 const githubUrl = import.meta.env.GITHUB_URL
 const commitSha = import.meta.env.GIT_COMMIT_SHA
+
+const downloadDB = async () => {
+  download(await exportDB(db), 'logicproblems.json', 'application/json')
+}
 </script>
 
 <template>
   <footer class="border-top py-3 mt-auto">
     <BContainer class="d-flex hstack flex-wrap justify-content-center justify-content-lg-end">
       <ul class="nav list-unstyled text-body-secondary hstack">
+        <li class="me-3">
+          <BButton size="sm" variant="outline-secondary" @click.prevent="downloadDB">Export Data</BButton>
+        </li>
         <li class="me-3 hstack">
           <a
             target="_new"
