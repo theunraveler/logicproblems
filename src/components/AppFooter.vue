bb92d2aa856265f00241f9c1932aa8077188c1c5
Jake Bell <jacobbell@fastmail.com>
2025-08-01 15:01:17 -0500

    Better responsive behavior for footer

--- src/components/AppFooter.vue
+++ src/components/AppFooter.vue
@@ -6,31 +6,27 @@ const commitSha = import.meta.env.GIT_COMMIT_SHA
 <template>
   <footer class="border-top py-3 mt-auto">
     <BContainer class="d-flex hstack flex-wrap justify-content-center justify-content-lg-end">
-      <ul class="nav list-unstyled text-body-secondary hstack">
-        <li class="me-3 hstack">
-          <a
-            target="_new"
-            class="text-body-secondary me-1"
-            href="https://creativecommons.org/licenses/by-sa/4.0/">
-            <IFa6BrandsCreativeCommons />
-            <IFa6BrandsCreativeCommonsBy />
-            <IFa6BrandsCreativeCommonsSa />
-          </a>
-          by Maran Wolston
-        </li>
-        <li class="me-3 hstack">
-          <a target="_new" class="text-body-secondary me-1" :href="githubUrl"><IBiGithub /></a>
-          <a
-            target="_new"
-            class="text-body-secondary text-decoration-none"
-            :href="`${githubUrl}/commit/${commitSha}`">
-            {{ commitSha }}
-          </a>
-        </li>
-        <li>
-          <ImportExportDatabase />
-        </li>
-      </ul>
+      <div class="hstack">
+        <a
+          target="_new"
+          class="text-body-secondary me-1"
+          href="https://creativecommons.org/licenses/by-sa/4.0/">
+          <IFa6BrandsCreativeCommons />
+          <IFa6BrandsCreativeCommonsBy />
+          <IFa6BrandsCreativeCommonsSa />
+        </a>
+        by Maran Wolston
+      </div>
+      <div class="ms-3 hstack">
+        <a target="_new" class="text-body-secondary me-1" :href="githubUrl"><IBiGithub /></a>
+        <a
+          target="_new"
+          class="text-body-secondary text-decoration-none"
+          :href="`${githubUrl}/commit/${commitSha}`">
+          {{ commitSha }}
+        </a>
+      </div>
+      <ImportExportDatabase class="mt-2 mt-sm-0 ms-sm-3" />
     </BContainer>
   </footer>
 </template>
