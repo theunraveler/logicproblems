e4961fd5f79c3801f0d439771d1fba21ed24b542
Jake Bell <jacobbell@fastmail.com>
2025-08-01 17:57:13 -0500

    Using color mode for app header

--- src/components/AppHeader.vue
+++ src/components/AppHeader.vue
@@ -22,12 +22,7 @@ const { store: colorMode } = useColorMode({
 
 <template>
   <header>
-    <BNavbar
-      container
-      toggleable="lg"
-      variant="dark"
-      data-bs-theme="dark"
-      class="border-bottom border-body">
+    <BNavbar container toggleable="lg" class="border-bottom border-body">
       <BNavbarBrand :to="{ name: 'home' }" data-testid="link-home">Logic Problems</BNavbarBrand>
       <BNavbarToggle target="nav-collapse" />
       <BCollapse id="nav-collapse" is-nav>
