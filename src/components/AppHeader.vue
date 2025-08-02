045ca5e2ec171bace02b9b2d9f83734428d5347e
Jake Bell <jacobbell@fastmail.com>
2025-08-02 00:42:57 -0500

    Setting `theme-color` meta var for pretty browsing

--- src/components/AppHeader.vue
+++ src/components/AppHeader.vue
@@ -1,6 +1,7 @@
 <script setup lang="ts">
-import { inject } from 'vue'
-import { useColorMode } from '@vueuse/core'
+import { computed, inject, useTemplateRef } from 'vue'
+import { useColorMode, useCssVar } from '@vueuse/core'
+import { useHead } from '@unhead/vue'
 import IBiSun from '~icons/bi/sun'
 import IBiMoon from '~icons/bi/moon'
 import IBiCircleHalf from '~icons/bi/circle-half'
@@ -13,16 +14,31 @@ const modes = {
   light: { icon: IBiSun, label: 'Light' },
   dark: { icon: IBiMoon, label: 'Dark' },
 }
-const { store: colorMode } = useColorMode({
+const { system: systemColorMode, store: storedColorMode } = useColorMode({
   selector: 'body',
   attribute: 'data-bs-theme',
   storageKey: 'color-theme',
+})
+const currentColorMode = computed(() => (
+  storedColorMode.value === 'auto' ? systemColorMode.value : storedColorMode.value
+))
+
+const navbar = useTemplateRef('navbar')
+const themeColor = computed(() => {
+  const varName = `--bs-${currentColorMode.value}-rgb`
+  return `rgb(${useCssVar(varName, navbar).value})`
+})
+
+useHead({
+  meta: [
+    { name: 'theme-color', content: themeColor },
+  ],
 })
 </script>
 
 <template>
   <header>
-    <BNavbar container toggleable="lg" class="border-bottom border-body">
+    <BNavbar container ref="navbar" :variant="currentColorMode" toggleable="lg" class="border-bottom border-body">
       <BNavbarBrand :to="{ name: 'home' }" data-testid="link-home">Logic Problems</BNavbarBrand>
       <BNavbarToggle target="nav-collapse" />
       <BCollapse id="nav-collapse" is-nav>
@@ -44,13 +60,13 @@ const { store: colorMode } = useColorMode({
           <BNavItem :to="{ name: 'about' }">About</BNavItem>
           <BNavItemDropdown>
             <template #button-content>
-              <Component :is="modes[colorMode].icon" />
+              <Component :is="modes[storedColorMode].icon" />
             </template>
             <BDropdownItem
               v-for="(m, name) in modes"
               :key="name"
-              @click="colorMode = name"
-              :link-class="['hstack', { active: colorMode === name }]">
+              @click="storedColorMode = name"
+              :link-class="['hstack', { active: storedColorMode === name }]">
               <Component :is="m.icon" class="me-2" />
               <span class="flex-grow-1">{{ m.label }}</span>
             </BDropdownItem>
